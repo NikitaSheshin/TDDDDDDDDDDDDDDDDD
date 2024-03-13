371f77978c2b4e913e9bc572dd65d5775e88cc38
NikitASheshiN <sheshin.nikita21@yandex.ru>
2024-03-13 16:03:17 +0300

    Добавил параметризированные тесты

--- src/test/java/DistanceTest.java
+++ src/test/java/DistanceTest.java
@@ -1,19 +1,33 @@
 import org.example.Point;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
 public class DistanceTest {
-    @Test(expected = IllegalArgumentException.class)
-    public void TestMethodCtorThrowsArgumentOutOfRangeException() {
-        Point p;
-        double a = -1000., b = -1000.;
+    private static Stream<Arguments> incorrectData() {
+        return Stream.of(
+                Arguments.of(-1000., -1000.),
+                Arguments.of(1000, 1000),
+                Arguments.of(91, 0),
+                Arguments.of(-91, 0),
+                Arguments.of(0, -181),
+                Arguments.of(0, 181)
+        );
+    }
 
-        p = new Point(a, b);
+    @ParameterizedTest
+    @MethodSource("incorrectData")
+    void testMethodWithAutoboxing(double a, double b) {
+        assertThrows(IllegalArgumentException.class, () -> new Point(a, b));
     }
 
     @Test
-    public void TestMethodCtorCreatePoint() {
+    public void testMethodCtorCreatePoint() {
         Point p;
         double a = 0., b = 0.;
 
