051f7b8f1402f6bab93f829c5bb3fdc9b9ebaf9a
NikitASheshiN <sheshin.nikita21@yandex.ru>
2024-03-13 14:55:14 +0300

    Новый тест добавил, не работает

--- src/test/java/DistanceTest.java
+++ src/test/java/DistanceTest.java
@@ -1,6 +1,8 @@
 import org.example.Point;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class DistanceTest {
     @Test(expected = IllegalArgumentException.class)
     public void TestMethodCtorThrowsArgumentOutOfRangeException() {
@@ -8,5 +10,14 @@ public class DistanceTest {
         double a = -1000., b = -1000.;
 
         p = new Point(a, b);
+    }
+
+    @Test
+    public void TestMethodCtorCreatePoint() {
+        Point p;
+        double a = 0., b = 0.;
+
+        p = new Point(a, b);
+        assertNotNull(p);
     }
 }
