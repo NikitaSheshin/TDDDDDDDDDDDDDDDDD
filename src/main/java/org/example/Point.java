cce4c6352e5c0e099f2ba97d5d0e8feee097c565
NikitASheshiN <sheshin.nikita21@yandex.ru>
2024-03-13 15:03:35 +0300

    Второй тест заработал(удивительно)

--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -1,8 +1,17 @@
 package org.example;
 
 public class Point {
+    double latitude;
+    double longitude;
+
     public Point(double latitude, double longitude)
     {
-        throw new IllegalArgumentException();
+        if(!(latitude >= -90 && latitude <= 90) ||
+                !(longitude >= -180 && longitude <= 180)) {
+            throw new IllegalArgumentException();
+        }
+
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 }
