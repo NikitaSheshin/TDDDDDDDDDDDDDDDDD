560fa75af1aaacb00fc5bdb06621021c613ba8db
NikitASheshiN <sheshin.nikita21@yandex.ru>
2024-03-13 15:30:37 +0300

    Добавил геттеры и сеттеры

--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -1,17 +1,36 @@
 package org.example;
 
 public class Point {
-    double latitude;
-    double longitude;
+    private double latitude;
+    private double longitude;
 
     public Point(double latitude, double longitude)
     {
-        if(!(latitude >= -90 && latitude <= 90) ||
-                !(longitude >= -180 && longitude <= 180)) {
-            throw new IllegalArgumentException();
+        setLatitude(latitude);
+        setLongitude(longitude);
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        if(!(latitude >= -90 && latitude <= 90)) {
+            throw new IllegalArgumentException("Некорректная широта");
+        }
+
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        if(!(longitude >= -180 && longitude <= 180)) {
+            throw new IllegalArgumentException("Некорректная долгота");
         }
 
         this.longitude = longitude;
-        this.latitude = latitude;
     }
 }
